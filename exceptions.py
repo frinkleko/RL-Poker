f28f0cbded6d3d54e0fa7431d91b3caef59ff995
frinkleko <frinkleko@gmail.com>
2023-01-11 16:51:09 +0800

    doc: add comments in exceptions

--- exceptions.py
+++ exceptions.py
@@ -1,14 +1,18 @@
 class startOver(Exception):
+    # This exception is raised when the user wants to start over
     pass
 
 
 class playerFold(Exception):
+    # This exception is raised when the player folds
     pass
 
 
 class comFold(Exception):
+    # This exception is raised when the computer folds
     pass
 
 
 class endTurn(Exception):
+    # This exception is raised when the player wants to end their turn
     pass
